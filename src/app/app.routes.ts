fa7818d18e5a13fc323956ab180647720d127483
Facundo Lopez <facundolopez99@gmail.com>
2025-11-02 22:18:12 -0300

    cambios de diseno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,18 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        pathMatch: 'full',
-        loadComponent: () => import('./home/home').then(m => m.Home)
+        loadComponent: () => import('./components/login/login').then(m => m.Login)
     },
     {
         path: 'login',
-        loadComponent: () => import('./login/login').then(m => m.Login)
+        loadComponent: () => import('./components/login/login').then(m => m.Login)
     },
     {
         path: 'registro',
-        loadComponent: () => import('./registro/registro').then(m => m.Registro)
+        loadComponent: () => import('./components/registro/registro').then(m => m.Registro)
     },
     {
         path: 'recuperarContrasenia',
-        loadComponent: () => import('./recuperar-contrasenia/recuperar-contrasenia').then(m => m.RecuperarContrasenia)
+        loadComponent: () => import('./components/recuperar-contrasenia/recuperar-contrasenia').then(m => m.RecuperarContrasenia)
     },
 ];
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink,  CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-  title = signal('Título de la Aplicación');
+  usuarioLogueado = signal(false);
+  menuAbierto = false;
+
+  cambioEstadoMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+  cerrarMenu() {
+    this.menuAbierto = false;
+  }
+
+  cerrarSesion() {
+    this.usuarioLogueado.set(false);
+    console.log('Sesión cerrada');
+  }
 }
