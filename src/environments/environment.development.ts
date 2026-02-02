28c2cdd33a72d9edbd25495cf4fa5e147bb8b8bb
Lautaro Mendez <lautaro_123mendez@hotmail.com>
2026-02-02 01:50:33 -0300

    Correcion URL API

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiURL: 'GestionAgendaAPI-env.eba-rpdj2hvw.us-east-1.elasticbeanstalk.com',
+    apiURL: 'GestionAgendaAPI-env.eba-rpdj2hvw.us-east-1.elasticbeanstalk.com/',
 };
