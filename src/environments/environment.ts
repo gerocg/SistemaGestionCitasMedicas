2f4c610790a5ee5ac323b3d5d68aaf5b722e8662
Gerónimo Castro <93690398+gerocg@users.noreply.github.com>
2026-03-09 22:01:33 -0300

    API URL actualizada para deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,3 @@
-// export const environment = {
-//   apiURL: 'http://GestionAgendaEntorno.eba-hunyzb3f.us-east-1.elasticbeanstalk.com'
-// };
-
 export const environment = {
-  production: true,
-  apiURL: '/api'  // ← relativa, ya no apunta directo a AWS
-};
+   apiURL: 'https://nka3ay0xqh.execute-api.us-east-1.amazonaws.com'
+ };
